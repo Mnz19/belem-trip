7a79e12933130b6acaa445f5e0ca07f58d996847
Thiago Menezes <thimenezes19@gmail.com>
2024-04-23 09:41:38 -0300

    fix: fix setting language

--- core/settings.py
+++ core/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "pr-br"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
